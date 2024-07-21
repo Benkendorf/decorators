b2761b84281113de1e5ac13c29210e838244aca2
Egor Korotaev <ego_kor@mail.ru>
2024-07-21 20:55:04 +0300

    Transform guess_number into a class-based program

--- guess_number.py
+++ guess_number.py
@@ -9,35 +9,34 @@ class NumberGame():
     def __init__(self) -> None:
         self.start_time = dt.now()
         self.username = self.get_username()
+        self.total_games = 0
 
     @access_control
-    def get_statistics(self, total_games: int, *args, **kwargs) -> None:
+    def get_statistics(self, *args, **kwargs) -> None:
         game_time = dt.now() - self.start_time
-        print(f'Общее время игры: {game_time}, текущая игра - №{total_games}')
-
+        print(f'Общее время игры: {game_time}, '
+              f'текущая игра - №{self.total_games}')
 
     @access_control
-    def get_right_answer(self, number: int, *args, **kwargs) -> None:
-        print(f'Правильный ответ: {number}')
+    def get_right_answer(self, *args, **kwargs) -> None:
+        print(f'Правильный ответ: {self.number}')
 
-
-    def check_number(self, username: str, guess: int, number: int) -> bool:
+    def check_number(self, guess: int) -> bool:
         # Если число угадано...
-        if guess == number:
-            print(f'Отличная интуиция, {username}! Вы угадали число :)')
+        if guess == self.number:
+            print(f'Отличная интуиция, {self.username}! Вы угадали число :)')
             # ...возвращаем True
             return True
 
-        if guess < number:
+        if guess < self.number:
             print('Ваше число меньше того, что загадано.')
         else:
             print('Ваше число больше того, что загадано.')
         return False
 
-
-    def game(self, username: str, total_games: int) -> None:
+    def game(self) -> None:
         # Получаем случайное число в диапазоне от 1 до 100.
-        number = randint(1, 100)
+        self.number = randint(1, 100)
         print(
             '\nУгадайте число от 1 до 100.\n'
             'Для выхода из текущей игры введите команду "stop"'
@@ -47,23 +46,21 @@ class NumberGame():
             # отрезаем лишние пробелы и переводим в нижний регистр.
             user_input = input('Введите число или команду: ').strip().lower()
 
-            match user_input:
-                case 'stop':
+            if user_input == 'stop':
+                break
+            elif user_input == 'stat':
+                self.get_statistics()
+            elif user_input == 'answer':
+                self.get_right_answer()
+            else:
+                try:
+                    guess = int(user_input)
+                except ValueError:
+                    print(UNKNOWN_COMMAND)
+                    continue
+
+                if self.check_number(guess):
                     break
-                case 'stat':
-                    get_statistics(total_games, username=username)
-                case 'answer':
-                    get_right_answer(number, username=username)
-                case _:
-                    try:
-                        guess = int(user_input)
-                    except ValueError:
-                        print(UNKNOWN_COMMAND)
-                        continue
-
-                    if check_number(username, guess, number):
-                        break
-
 
     def get_username(self) -> str:
         username = input('Представьтесь, пожалуйста, как Вас зовут?\n').strip()
@@ -76,14 +73,13 @@ class NumberGame():
             print(f'\n{username}, добро пожаловать в игру!')
         return username
 
-
     def guess_number(self) -> None:
-        #username = get_username()
+        # username = get_username()
         # Счётчик игр в текущей сессии.
-        total_games = 0
+
         while True:
-            total_games += 1
-            game(username, total_games)
+            self.total_games += 1
+            self.game()
             play_again = input('\nХотите сыграть ещё? (yes/no) ')
             if play_again.strip().lower() not in ('y', 'yes'):
                 break
@@ -94,4 +90,5 @@ if __name__ == '__main__':
         'Вас приветствует игра "Угадай число"!\n'
         'Для выхода нажмите Ctrl+C'
     )
-    guess_number()
+    new_game = NumberGame()
+    new_game.guess_number()
